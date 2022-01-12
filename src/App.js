095bdf1ad1e777e9fdb42150d376a9c98ad74f63
wuguoxun <william.wu487@gmail.com>
2022-01-12 11:55:09 -0800

    install and setup router

--- src/App.js
+++ src/App.js
@@ -3,23 +3,50 @@ import MoviePage from "./components/MoviePage/MoviePage"
 import { connect } from 'react-redux';
 import { actions } from "./actionCreator"
 import { useEffect } from 'react';
+import React from "react"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App(props) {
 
-  useEffect(()=>{
+  useEffect(() => {
     props.getMovie();
-  },[])
+  }, [])
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(props.movie)
-  },[props.movie])
+  }, [props.movie])
 
   return (
-    <div>
-      <MoviePage 
-        movie = {props.movie}
-      />
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Movie</Link>
+        </li>
+        <li>
+          <Link to="/page2">page2</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path='/' element=
+          {<MoviePage
+            movie={props.movie}
+          />}
+        />
+        <Route path='/page2' element=
+          {
+            <div>Hello You Can Place Page2 Here</div>
+          }
+        />
+      </Routes>
+      <div>
+      </div>
+    </Router>
+
   );
 }
 
@@ -29,7 +56,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    getMovie: ()=> dispatch(actions.getMovie()),
+    getMovie: () => dispatch(actions.getMovie()),
   }
 };
 
